20ae846c291b81df7d2746c52df5fe9180fe09f8
Mullan, Danny <dannymullan17@gmail.com>
2022-08-25 22:06:13 +0100

    knockout code added an empty list retrieved

--- NovaFori/wwwroot/js/todo.js
+++ NovaFori/wwwroot/js/todo.js
@@ -1,1 +1,40 @@
-﻿
+﻿const uri = 'api/todoitems';
+
+var TodoViewModel = function () {
+
+    var self = this;
+
+    //Declare observable which will be bind with UI  
+    self.Description = ko.observable("");
+    self.IsComplete = ko.observable(false);
+
+    //The Object which stored data entered in the observables  
+    var TodoData = {
+        Description: self.Description,
+        IsComplete: self.IsComplete
+    };
+
+    //Declare 2 ObservableArrays for storing both types of todos 
+    self.PendingTodos = ko.observableArray([]);
+    self.CompletedTodos = ko.observableArray([]);
+
+    //Call the Function which gets all todos using ajax call  
+    GetTodoList();
+
+    function GetTodoList() {
+        $.ajax({
+            type: "GET",
+            url: uri,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                self.PendingTodos(self.TodoList().filter(x => !x.isComplete));
+                self.CompletedTodos(self.TodoList().filter(x => x.isComplete));
+            },
+            error: function (error) {
+                console,log(error.status + "<--and--> " + error.statusText);
+            }
+        });
+    }
+};
+ko.applyBindings(new TodoViewModel());
